a3dfe26eb9851c23e1ab93462e64a99f03e6f71a
Uri Dvir <uri.dvir@microfocus.com>
2023-05-14 09:27:04 +0300

    Class 17

--- app/src/main/java/com/example/firstfirebaseapp/ChatActivity.java
+++ app/src/main/java/com/example/firstfirebaseapp/ChatActivity.java
@@ -5,24 +5,38 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
     GoogleSignInAccount account;
     MessageAdapter adapter;
 
+    RequestQueue mRequestQue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mRequestQue = Volley.newRequestQueue(this);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -35,11 +49,27 @@ public class ChatActivity extends AppCompatActivity {
         ImageView userImage = findViewById(R.id.mainUserImage);
         Glide.with(this).load(account.getPhotoUrl()).into(userImage);
 
-        adapter = new MessageAdapter();
+        adapter = new MessageAdapter(account.getId());
         RecyclerView recycler = findViewById(R.id.chatRV);
         recycler.setHasFixedSize(false);
         RecyclerView.LayoutManager manager = new GridLayoutManager(getApplicationContext(),1);
         recycler.setLayoutManager(manager);
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                recycler.smoothScrollToPosition(adapter.getItemCount()-1);
+            }
+        });
+
+        recycler.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
+                if (adapter.getItemCount()>0) {
+                    recycler.smoothScrollToPosition(adapter.getItemCount()-1);
+                }
+            }
+        });
 
         recycler.setAdapter(adapter);
 
@@ -51,8 +81,44 @@ public class ChatActivity extends AppCompatActivity {
                 chatMessage m = new chatMessage(account.getPhotoUrl().toString(),account.getDisplayName(),account.getId(),text.getText().toString());
                 adapter.addMessage(m);
                 text.setText("");
+                sendCloudMessage(m);
             }
         });
 
+    }
+
+    private void sendCloudMessage(chatMessage m) {
+        String serverKey = "AAAAVKKMYTk:APA91bGY5kzO-5NYqRCU-DAu2rJdF1fLnLImjbDLj8JCk8pBGN2ahHIcv8hAX2woGmCdFfdaha0m5NsQpZmExZUZK15W1aahu2Orv_9TTibBYXDQn03bC76Rivuiq9qdOfdqC6Z2uYrO";
+        String topic = "chat";
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("to", "/topics/" + topic);
+            JSONObject notificationObj = new JSONObject();
+            notificationObj.put("title", m.userName);
+            notificationObj.put("body", m.message);
+            json.put("notification", notificationObj);
+
+            String URL = "https://fcm.googleapis.com/fcm/send";
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,URL,
+                    json,
+                    response -> Log.d("MUR", "onResponse: " + response.toString()),
+                    error -> Log.d("MUR", "onError: " + error.networkResponse)
+            ) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> header = new HashMap<>();
+                    header.put("content-type", "application/json");
+                    header.put("authorization", "key=" + serverKey);
+                    return header;
+                }
+            };
+
+            mRequestQue.add(request);
+
+        } catch (Exception e) {
+
+        }
     }
 }
--- app/src/main/java/com/example/firstfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firstfirebaseapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.firstfirebaseapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,8 +17,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("chat");
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
